1e4ebc9d1070f071e655e6315d902f189087b841
Ivan Kashtanov <kashtanov@qnium.com>
2017-06-13 15:10:19 +0300

    Fixes for making QRTEControl work without additional setup.
    Added CKEditor dependency for QRTEControl.
    Added modified CKEditor wrapper that uses imported CKEDITOR variable.

--- src/components/QRTEControl.js
+++ src/components/QRTEControl.js
@@ -1,9 +1,69 @@
 import React from 'react';
-import CKEditor from 'react-ckeditor-wrapper';
+import ReactDOM from "react-dom";
+import CKEDITOR from 'ckeditor';
+//import CKEditor from 'react-ckeditor-wrapper';
 
 const DEFAULT_EDITOR_CONFIG = {
 
 };
+
+class CKEditor extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: props.value,
+            config: props.config || {},
+            onChange: props.onChange
+        };
+    }
+
+    handleChange() {
+        this.state.onChange(this.state.value);
+    }
+
+    componentDidMount() {
+        /*if (!window.CKEDITOR) {
+            console.error("CKEditor not found");
+            return;
+        }*/
+
+        this.instance = CKEDITOR.appendTo(
+            ReactDOM.findDOMNode(this),
+            this.state.config,
+            this.state.value
+        );
+        this.instance.on("change", () => {
+            this.state.value = this.instance.getData();
+            this.handleChange();
+        });
+    }
+
+  componentWillReceiveProps(props) {
+    if (!this.instance) {
+        return;
+    }
+
+    if (this.state.value !== props.value) {
+        // setData will move the cursor to the begining of the input
+        this.instance.setData(props.value);
+    }
+
+    if (props.config && this.state.config !== props.config) {
+        if ("readOnly" in props.config)
+        this.instance.setReadOnly(props.config.readOnly);
+    }
+
+    this.setState({
+        value: props.value,
+        config: props.config || {},
+        onChange: props.onChange
+    });
+  }
+
+    render() {
+        return <div />;
+    }
+}
 
 class QRTEControl extends React.Component
 {
